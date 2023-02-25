2035b868d6d659147c9fd6db85b3146b476b87ac
Arsemy <arsemy2@gmail.com>
2023-02-25 09:24:12 -0300

    refactor(pset2): remove duplicated "define"

--- week02/pset2/wordle/wordle.c
+++ week02/pset2/wordle/wordle.c
@@ -12,9 +12,6 @@
 #define CLOSE 1
 #define WRONG 0
 
-// values for colors and score (EXACT == right letter, right place; CLOSE == right letter, wrong place; WRONG == wrong letter) #define EXACT 2 #define CLOSE 1
-#define WRONG 0
-
 // ANSI color codes for boxed in letters
 #define GREEN   "\e[38;2;255;255;255;1m\e[48;2;106;170;100;1m"
 #define YELLOW  "\e[38;2;255;255;255;1m\e[48;2;201;180;88;1m"
