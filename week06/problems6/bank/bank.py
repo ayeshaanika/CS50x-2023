47194b144f5e4d25e602153c7e4ed10464f3028d
Arsemy <arsemy2@gmail.com>
2023-03-16 15:53:12 -0300

    refactor(problems6): don't check the same letter again

--- week06/problems6/bank/bank.py
+++ week06/problems6/bank/bank.py
@@ -2,7 +2,8 @@ def main():
     greeting = input('Greeting: ').strip().lower()
     hello = 'hello'
     if greeting[0] == 'h':
-        for greeting_letter, hello_letter in zip(greeting, hello):
+        for greeting_letter, hello_letter in zip(greeting[1:], hello[1:]):
+            print(greeting_letter)
             if (greeting_letter != hello_letter):
                 print('$20')
                 return
